b03d7024245a1bdbb3c32ec5c7726119f8bb2962
Mathioussee <matcap31@gmail.com>
2023-12-27 13:55:57 +0100

    1.0.4 - Now messages are slugified the caps work on detection

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -20,79 +20,6 @@ const slugify = (text) => {
         .replace(/\-$/g, '');         // Remove trailing -
 }
 
-// client.on('messageCreate', async message => {
-//     if (message.author.bot) return;
-//     if (message.channel.type === 'DM') return;
-
-//     if (!messagesCache[message.channelId]) {
-//         messagesCache[message.channelId] = [message];
-//     } else {
-//         messagesCache[message.channelId].push(message);
-//     }
-//     if (messagesCache[message.channelId].length > 10) {
-//         messagesCache[message.channelId].shift();
-//     }
-
-//     try {
-//         for (const word of coubehWords) {
-//             if (!message.content) continue
-//             if (message.content.includes(word)) {
-//                 for (const messages of messagesCache[message.channelId]) {
-//                     if (messages.author.id === message.author.id) { continue }
-//                     if (regexPrankex.test(slugify(messages.content))) {
-//                         console.log(message.content)
-
-//                         const isMessagePresent = await messagesCoubeds.findOne({ where: { messageId: messages.id, guildId: message.guild.id } });
-//                         if (isMessagePresent) {
-//                             continue
-//                         }
-//                         await messagesCoubeds.upsert({ messageId: messages.id, coubedBy: messages.author.id, guildId: message.guild.id });
-//                         const userStats = await userCoubehStats.findOne({ where: { discordid: message.author.id, guildId: message.guild.id } })
-
-//                         if (!userStats) {
-//                             const userStats = { discordid: message.author.id, username: message.author.globalName, quoicoubehCount: 1, guildId: message.guild.id }
-//                             userCoubehStats.upsert(userStats)
-//                         } else {
-//                             userStats.quoicoubehCount++
-//                             userStats.save()
-//                         }
-//                         message.react('👏')
-
-//                         const leaderboardMessageRecord = await leaderboardMessages.findOne({ where: { guildId: message.guild.id } });
-//                         if (leaderboardMessageRecord) {
-//                             const channel = client.channels.cache.get(leaderboardMessageRecord.channelId);
-//                             let leaderboardMessage
-//                             try {
-//                                 leaderboardMessage = await channel.messages.fetch(leaderboardMessageRecord.messageId);
-//                             } catch (error) {
-//                                 console.error(`Failed to fetch message: ${error}`);
-//                             }
-//                             let ranks = []
-//                             const generalStats = await userCoubehStats.findAll({ where: { guildId: message.guild.id } })
-//                             generalStats.forEach(stat => {
-//                                 const discordid = stat.dataValues.discordid
-//                                 const quoicoubehCount = stat.dataValues.quoicoubehCount
-//                                 ranks.push({ discordid: discordid, quoicoubehCount: quoicoubehCount })
-//                             });
-//                             function compareByCount(a, b) {
-//                                 return a.quoicoubehCount + b.quoicoubehCount;
-//                             }
-//                             ranks.sort(compareByCount);
-//                             const embed = new EmbedBuilder()
-//                                 .setTitle('Leaderboard')
-//                                 .setDescription('This is the leaderboard');
-
-//                             await leaderboardMessage.edit({ embeds: [embed] });
-//                         }
-//                     } else {
-//                         console.log("no match for word '" + word, messages.content.toLowerCase(), "'")
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// )
-
 client.on('messageCreate', async message => {
     if (message.author.bot) return;
     if (message.channel.type === 'DM') return;
@@ -109,7 +36,7 @@ client.on('messageCreate', async message => {
     try {
         for (const word of coubehWords) {
             if (!message.content) continue
-            if (message.content.includes(word)) {
+            if (slugify(message.content).includes(word)) {
                 for (const messages of messagesCache[message.channelId]) {
                     if (messages.author.id === message.author.id) { continue }
                     if (regexPrankex.test(slugify(messages.content))) {
